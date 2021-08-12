a93be5379e1f074857d9a4f4cb1f49e224e6c206
B6Infinity <broteendas.official@gmail.com>
2021-08-12 13:29:07 +0530

    setting up the db

--- admin/admin.py
+++ admin/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Profile
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Product)
--- admin/models.py
+++ admin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+class Profile(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
