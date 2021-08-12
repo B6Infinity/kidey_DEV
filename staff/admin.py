fc99b66a22ea0d8546091ce40ab6d7561f37ecb8
B6Infinity <broteendas.official@gmail.com>
2021-08-12 15:21:59 +0530

    database tables created

--- staff/admin.py
+++ staff/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Product, Profile, Order
+from .models import Product, Profile, Order, Expense, Customer
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Expense)
+admin.site.register(Customer)
--- staff/models.py
+++ staff/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.base import Model
 
 # Create your models here.
 class Product(models.Model):
@@ -12,6 +13,7 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name} - ₹{self.price}"
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -23,15 +25,54 @@ class Profile(models.Model):
             stfsts = "(STAFF)"
         return f"{str(self.user.first_name)} {str(self.user.last_name)} - Profile {stfsts}"
 
+
 class Order(models.Model):
-    products = models.ManyToManyField(Product, related_name='orders')
+    custormer_phone_no = models.IntegerField(max_length=15)
+
+    order_json = models.JSONField(null=True) # {"<Product ID>": <No. of items>} eg. {"2": 3, "1": 2}
     time_of_order = models.DateTimeField()
     time_of_delivery = models.DateTimeField()
 
+    time_created = models.DateTimeField(auto_now_add=True)
+
     total_bill = models.IntegerField(default=0)
+    bill_text = models.TextField(default="", blank=True)
 
     discount = models.IntegerField(default=0)
 
-    def __str__(self) -> str:
-        return f"{len(self.products)} Products - ₹{self.total_bill} - ₹{self.discount}"
     
+
+    def save(self, *args, **kwargs):
+
+        bill = 0
+        bill_text = ''
+
+        for product_id in self.order_json:
+            product = Product.objects.get(id=product_id)
+            bill += product.price * self.order_json[product_id]
+
+            bill_text += f'{product.name} (₹{product.price}) [x{self.order_json[product_id]}] - ₹{product.price * self.order_json[product_id]}\n'
+
+        self.total_bill = bill
+        self.bill_text = bill_text
+
+
+        super(Order, self).save(*args, **kwargs)
+    
+    
+    def __str__(self) -> str:
+        return f"₹{self.total_bill} - ₹{self.discount} >> {(self.time_of_delivery.strftime('%d/%m/%y, %H:%M'))}"
+
+
+class Expense(models.Model):
+
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_of_expense = models.DateTimeField()
+
+    amount = models.IntegerField()
+
+    summary = models.CharField(max_length=150, null=True, blank=True)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=70)
+    phone_no = models.IntegerField(max_length=15)
