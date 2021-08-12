7b422c3ded7e2a395d3ebc8589b79974c4857c13
B6Infinity <broteendas.official@gmail.com>
2021-08-12 13:11:28 +0530

    first commit

---
+++ admin/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    
+    path('', view=views.home, name='sudohome'),
+
+
+
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
