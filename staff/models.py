3307b0c8681306d0f6f0341a30f7a30df2203db0
B6Infinity <broteendas.official@gmail.com>
2021-08-12 14:21:49 +0530

    updated timezone to 'Asia/Kolkata'

--- staff/models.py
+++ staff/models.py
@@ -10,6 +10,9 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f"{self.name} - ₹{self.price}"
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     is_staff = models.BooleanField(default=False)
@@ -25,4 +28,10 @@ class Order(models.Model):
     time_of_order = models.DateTimeField()
     time_of_delivery = models.DateTimeField()
 
+    total_bill = models.IntegerField(default=0)
+
+    discount = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"{len(self.products)} Products - ₹{self.total_bill} - ₹{self.discount}"
     
