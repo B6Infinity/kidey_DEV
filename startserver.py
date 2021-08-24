0b74ab7844543530b971aa3fcb92994ecd034b7d
B6Infinity <broteendas.official@gmail.com>
2021-08-24 21:49:33 +0530

    nicher push

--- startserver.py
+++ startserver.py
@@ -3,9 +3,11 @@ import os
 import os, sys, platform
 import datetime
 
+origin = 'https://github.com/B6Infinity/kidey_DEV'
+
 # PULLING CHANGES
 print("\n\n\n\033[95m\033[1m\033[4mSYNCHRONISING DATABASE...\033[0m\n\n\n")
-os.system('git pull origin master')
+os.system('git pull https://github.com/B6Infinity/kidey_DEV master')
 
 
 # GETTING THE IP ADDRESS OF NATIVE MACHINE (LAN COMPATIBLE)
@@ -36,7 +38,7 @@ finally:
 
     os.system('git add db.sqlite3')
     os.system(f'git commit -m "Changed db.sqlite3 from {native_ip} at {datetime.datetime.now().date()}--{datetime.datetime.now().time()}"')
-    os.system('git push origin master')
+    os.system('git push https://github.com/B6Infinity/kidey_DEV master')
 
     
     
